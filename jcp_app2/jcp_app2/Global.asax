60244ffcffc95f8814c352d94cf2e46d6d601405
Jerry Pierre <tribalheadzsolutions@gmail.com>
2016-02-05 14:15:57 -0800

    first commit.

---
+++ jcp_app2/jcp_app2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jcp_app2.Global" Language="C#" %>
---
+++ jcp_app2/jcp_app2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jcp_app2.Startup))]
+namespace jcp_app2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
